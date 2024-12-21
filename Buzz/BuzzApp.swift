7712f357e920ec6af3a94d2244f2399c2c4d009d
Alfayed Baksh <baksha97@gmail.com>
2024-12-21 11:08:32 -0500

    Initial Commit

---
+++ Buzz/BuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzApp.swift
+//  Buzz
+//
+//  Created by Travis Baksh on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuzzTests/BuzzTests.swift
@@ -0,0 +1,16 @@
+//
+//  BuzzTests.swift
+//  BuzzTests
+//
+//  Created by Travis Baksh on 12/21/24.
+//
+
+import Testing
+
+struct BuzzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
